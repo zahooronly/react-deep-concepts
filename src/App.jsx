8d9be4bfae052c43dbcf4897f1b9d16702c16676
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-28 15:22:11 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import { useEffect, useState } from "react";
+import "./index.css";
+
+export default function App() {
+  // const [resourceType, setResourceType] = useState("posts");
+  // const [items, setItems] = useState();
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setItems(json));
+  // }, [resourceType]);
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
+  // }, []);
+
+  return (
+    <>
+      {/* <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items?.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+        })} */}
+      {/*<div>{windowWidth}</div>*/}
+    </>
+  );
+}
